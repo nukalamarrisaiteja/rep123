2363b3610923c1bc50a8fdde4603cada6d72450c
nukalamarrisaiteja <145985039+nukalamarrisaiteja@users.noreply.github.com>
2024-01-27 10:14:44 +0530

    added project files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hanman</h1>
+    <img src="./hanmanimages/Hanman.webp"></img>
+    </div>
+  );
+}
+
+export default App;
